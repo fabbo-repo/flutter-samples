395c296d12383b194d222ee5b6c3689d5a4f79fe
Fabián Scherle <fscherle98@gmail.com>
2022-07-17 16:49:21 +0200

    Update djanggo and flutter

---
+++ Flutter_DjangoRest/django_crud_example/employee/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from employee.models import Employee
+from employee.serializers import EmployeeSerializer
+from rest_framework import viewsets
+
+class Employee(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
